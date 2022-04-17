c10ac1eb75d7000ebaa9c586d6c07ad9f3929cf9
krutoi-muzhik <ddaniild@list.ru>
2022-04-17 05:54:51 +0300

    finding all functions

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 from Python3Listener import Python3Listener
  
-def main(argv):
-    input_stream = FileStream(argv[1])
-    lexer = Python3Lexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = Python3Parser(stream)
-    tree = parser.startRule()
-    enterer = FuncEnterer()
-    walker = ParseTreeWalker()
-    walker.walk(printer, tree)
- 
 class FuncEnterer (Python3Listener):
     def enterFuncdef(self, ctx:Python3Parser.FuncdefContext):
-        print (self)
+        print ("entered ", ctx.NAME())
+ 
+def main(argv):
+    input_stream = FileStream (argv[1])
+    lexer = Python3Lexer (input_stream)
+    stream = CommonTokenStream (lexer)
+    parser = Python3Parser (stream)
+    tree = parser.file_input ()
+    enterer = FuncEnterer ()
+    walker = ParseTreeWalker ()
+    walker.walk (enterer, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
