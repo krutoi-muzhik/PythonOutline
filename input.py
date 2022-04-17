00637da99cf29430d41022dde3d17dfa5a3b20b0
krutoi-muzhik <ddaniild@list.ru>
2022-04-17 06:11:30 +0300

    listener finds funcs nd classes

--- input.py
+++ input.py
@@ -7,6 +7,11 @@ def GetResponceText (url):
 	text = responce.text
 	return text
 
+class ABOBA:
+	def biba ():
+		pass
+	boba = 1
+
 def main ():
 	url = "https://google.com"
 	print (GetResponceText (url))
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 from Python3Listener import Python3Listener
  
-class FuncEnterer (Python3Listener):
+class FuncClassHandler (Python3Listener):
     def enterFuncdef(self, ctx:Python3Parser.FuncdefContext):
-        print ("entered ", ctx.NAME())
+        print ("function ", ctx.NAME())
+        
+    def enterClassdef(self, ctx:Python3Parser.ClassdefContext):
+        print ("class ", ctx.NAME())
  
 def main(argv):
     input_stream = FileStream (argv[1])
@@ -14,9 +17,10 @@ def main(argv):
     stream = CommonTokenStream (lexer)
     parser = Python3Parser (stream)
     tree = parser.file_input ()
-    enterer = FuncEnterer ()
+
+    handler = FuncClassHandler ()
     walker = ParseTreeWalker ()
-    walker.walk (enterer, tree)
+    walker.walk (handler, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
