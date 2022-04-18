4f4178446d7eaf84f6bae6cb3a49b8bcff307035
krutoi-muzhik <ddaniild@list.ru>
2022-04-18 04:27:44 +0300

    fully workong

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ class CustomListener (Python3Listener):
         if self.IN == 0:
             print (self.pathname, ctx.DEF(), ctx.NAME())
 
+            print ("ARROW = ", ctx.ARROW(), "")
+
             #self.cursor.execute ("SELECT file FROM objects")
             self.cursor.execute ("INSERT INTO objects (file, type, name) VALUES (\'{}\', \'{}\', \'{}\')".format (self.pathname, ctx.DEF(), ctx.NAME()))
             self.db.commit ()
@@ -56,8 +58,8 @@ class CustomListener (Python3Listener):
         self.IN -= 1
 
 
-def initDB ():
-    db = sqlite3.connect ('objects.db')
+def initDB (pathname):
+    db = sqlite3.connect (pathname)
     cursor = db.cursor ()
 
     cursor.execute ("""CREATE TABLE IF NOT EXISTS objects (file TEXT, type TEXT, name TEXT)""")
@@ -66,32 +68,12 @@ def initDB ():
     return db, cursor
 
 
-# def FillBase (db, pathname):
-#     input_stream = FileStream (pathname)
-#     lexer = Python3Lexer (input_stream)
-#     token_stream = CommonTokenStream (lexer)
-#     parser = Python3Parser (token_stream)
-#     tree = parser.file_input ()
-
-#     handler = CustomListener ()
-#     handler.IN = 0
-#     walker = ParseTreeWalker ()
-#     walker.walk (handler, tree)
-
-#     cursor = db.cursor ()
-#     cursor.execute ("SELECT file FROM objects")
-#     cursor.execute ("INSERT INTO objects VALUES ('{path}')")
-
-
 def PrintDB (cursor):
     for value in cursor.execute ("SELECT * FROM objects"):
         print (value)
 
 
-def main ():
-    db, cursor = initDB ()
-
-    pathname = input ()
+def ListenFile (pathname, db, cursor):
     input_stream = FileStream (pathname)
     lexer = Python3Lexer (input_stream)
     stream = CommonTokenStream (lexer)
@@ -103,10 +85,19 @@ def main ():
     walker = ParseTreeWalker ()
     walker.walk (handler, tree)
 
+
+
+def main ():
+    db, cursor = initDB ('objects.db')
+
+    paths = input ()
+    for value in paths.split(" "):
+        if value != "":
+            ListenFile (value, db, cursor)
+
     PrintDB (cursor)
 
     db.close ()
-
 
 
 
